99e5230eb1ddf6fbd8808643d996cba8ba43e206
quagh03 <03.quanghuy.le@gmail.com>
2023-12-18 12:47:52 +0700

    init

---
+++ src/main/java/org/example/ecommercebackend/controller/democontroller.java
@@ -0,0 +1,14 @@
+package org.example.ecommercebackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class democontroller {
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
